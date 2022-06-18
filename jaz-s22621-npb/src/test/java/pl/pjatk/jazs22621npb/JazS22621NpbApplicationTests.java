f78f6e8353cdf90587d59fbc3712f771142aa36f
dawidowskilukasz <73032711+dawidowskilukasz@users.noreply.github.com>
2022-06-18 11:58:28 +0200

    Add files via upload

---
+++ jaz-s22621-npb/src/test/java/pl/pjatk/jazs22621npb/JazS22621NpbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs22621npb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS22621NpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
