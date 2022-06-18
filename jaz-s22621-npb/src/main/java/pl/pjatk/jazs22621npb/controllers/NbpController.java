1377f5d403664ac47ad523d4d103dd405a8d7e92
dawidowskilukasz <73032711+dawidowskilukasz@users.noreply.github.com>
2022-06-18 13:27:32 +0200

    Add files via upload

---
+++ jaz-s22621-npb/src/main/java/pl/pjatk/jazs22621npb/controllers/NbpController.java
@@ -0,0 +1,28 @@
+package pl.pjatk.jazs22621npb.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.pjatk.jazs22621npb.services.NbpService;
+
+@RestController
+@RequestMapping("/nbp")
+public class NbpController {
+
+    private final NbpController nbpController;
+
+    public NbpController (NbpController nbpController) {
+        this.nbpController = nbpController;
+    }
+}
+
+@GetMapping("/srednia/{datestart}/{dateend}/")
+
+    public ResponseEntity<NbpQuery> getAverageValue(@PathVariable String datestart, @PathVariable String dateend){
+    return ResponseEntity.ok(nbp)
+}
+
+
+
---
+++ jaz-s22621-npb/src/main/java/pl/pjatk/jazs22621npb/interfaces/NbpRepository.java
@@ -0,0 +1,6 @@
+package pl.pjatk.jazs22621npb.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NbpRepository extends JpaRepository {
+}
